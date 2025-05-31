b171c03fa67a1a56f1b8206dedb07c3e5791ad9d
Fahmi Affan <fahmi.affan@merkleinnovation.co.id>
2025-05-31 09:10:06 +0700

    fix react routes about

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -4,11 +4,9 @@ interface AboutPageProps {
   title: string;
   description: string;
 }
-export default function AboutPage({ title, description }: AboutPageProps) {
+export default function AboutPage() {
   return (
     <div className="about-page">
-      <h1>{title}</h1>
-      <p>{description}</p>
       <p>This is the about page where you can learn more about us.</p>
       <p>We are dedicated to providing the best service possible.</p>
     </div>
