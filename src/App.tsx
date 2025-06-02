8c246a418a09b31f471242cd3c8c48cf056c1326
Fahmi Affan <fahmi.affan@merkleinnovation.co.id>
2025-06-02 21:19:27 +0700

    setup iconify

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import { Icon } from "@iconify/react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+    <Icon icon={"mdi-light:home"} />
       <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
     </>
   );
